aa9622ef44d0140d5dffa345c39a114e24ecfeb9
Anh Nguyen <an7s@virginia.edu>
2023-01-21 23:03:56 +0000

    Cleanup of rq scripts

--- experiments/scripts/rq1.sh
+++ experiments/scripts/rq1.sh
@@ -1,4 +1,18 @@
+if [ ! -z $1 ]; then
+subjects="$@"
+
+else
 subjects="commons-cli commons-codec-1.12 commons-csv commons-validator commons-lang-3.6 jaxen-1.2.0 joda-time gson jackson-dataformat-xml jfreechart jsoup-1.10.1 plexus-utils xstream"
+fi
+
+# make sure subjects exist
+for s in $subjects
+do
+	if [ ! -d "$HCC_EXPERIMENTS/subjects/$s" ]; then
+		echo "subject program $s cannot be found"
+		exit 1
+	fi
+done
 
 env | grep HCC_HOME
 if [ ! $? -eq 0 ]; then
