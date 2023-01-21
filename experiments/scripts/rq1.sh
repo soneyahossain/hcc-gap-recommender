cb3278a6270f526e694ed7a2bef021daf86b6c44
Anh Nguyen <an7s@virginia.edu>
2023-01-21 21:18:42 +0000

    Commit rq1.sh script

--- experiments/scripts/rq1.sh
+++ experiments/scripts/rq1.sh
@@ -1,5 +1,12 @@
 subjects="commons-cli commons-codec-1.12 commons-csv commons-validator commons-lang-3.6 jaxen-1.2.0 joda-time gson jackson-dataformat-xml jfreechart jsoup-1.10.1 plexus-utils xstream"
 
+env | grep HCC_HOME
+if [ ! $? -eq 0 ]; then
+	echo Error: environment variable HCC_HOME is not set
+	echo Please look at installation instruction to set HCC_HOME and other needed environment variables
+	exit 1
+fi
+
 date
 echo "Compute baseline statement and object-branch coverage for subjects: $subjects"
 for s in $subjects
@@ -14,3 +21,14 @@ for s in $subjects
 do
 	./compute_hcc_from_slices.sh $s	
 done
+
+echo
+date
+echo "=== Show HCC statement (SCC) output for each subject"
+ls $HCC_EXPERIMENTS/hcc_results/*/scc.csv
+
+echo
+date
+echo "=== Show HCC object-branch (OBCC) output for each subject"
+ls $HCC_EXPERIMENTS/hcc_results/*/obcc.csv
+
